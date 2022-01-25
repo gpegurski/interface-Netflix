17ddfe35123287016f711a5653f7b5d933b45f79
gpegurski <grazipegurski@gmail.com>
2022-01-25 18:01:17 -0300

    Add carrossel times europeus

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -14,3 +14,19 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
